e5e17e29c179b83b407ec1605be99c1475cf5080
pannaincze <pannaincze@gmail.com>
2023-05-30 15:16:24 +0200

    create basic server side

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001/') // update to match the domain you will make the request from
+  res.header('Access-Controll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+mongoose.connect("mongodb+srv://pannaincze:8LrZGgipeY9veHEy@cluster0.6y94z96.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+app.listen(3000, () => console.log('Server started on port 3000'))
